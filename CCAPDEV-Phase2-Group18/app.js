4be5e54c6931f46432f0495dc8ab3ea0c9e5c626
dreadulin <73688919+dreadulin@users.noreply.github.com>
2024-03-08 01:03:14 +0800

    revise db code + add data retrieval from db

--- CCAPDEV-Phase2-Group18/app.js
+++ CCAPDEV-Phase2-Group18/app.js
@@ -1,15 +1,10 @@
-/* Install commands: 
+/*Install Command:
     npm init
-    npm i express express-handlebars body-parser
+    npm i express express-handlebars body-parser mongoose
 */
+
 const express = require('express');
 const server = express();
-
-// for DB connection: 
-const connectDB = require("./dbConnect.js");
-
-//calling the DB connector 
-connectDB();
 
 const bodyParser = require('body-parser')
 server.use(express.json()); 
@@ -24,11 +19,87 @@ server.engine('hbs', handlebars.engine({
 
 server.use(express.static('public'));
 
+//for the DB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://user:appdevmco2@cluster0.gysra2q.mongodb.net/mco');
+
+// schemas
+const commentSchema = new mongoose.Schema({
+    upvote: { type: Number }, 
+    downvote: {type: Number},
+    content: {type: String},
+    authorid: {type: Number}, 
+    dateposted: {type: String}
+},{ versionKey: false });
+
+const cafeSchema = new mongoose.Schema({
+   cafeid: { type: Number }, 
+   cafename: {type: String},
+   ownerid: { type: Number }, 
+   logo: {type: String},
+   rating: {type: Number}
+},{ versionKey: false });
+
+const postSchema = new mongoose.Schema({
+    authorid: {type: Number},
+    createdate: {type: String},
+    updatedate: {type: String},
+    dateposted: {type: String},
+    upvote: { type: Number }, 
+    downvote: {type: Number},
+    title: {type: String},
+    description: {type: String},
+    image: {type: String}
+ },{ versionKey: false });
+
+ const userSchema = new mongoose.Schema({
+    userid: { type: Number }, 
+    username: {type: String},
+    password: { type: Number }, 
+    profpic: {type: String},
+    joindate: {type: String},
+    isowner: {type: Boolean}
+ },{ versionKey: false });
+
+// models
+const commentModel = mongoose.model('comment',commentSchema);
+const cafeModel = mongoose.model('cafe',cafeSchema);
+const postModel = mongoose.model('post',postSchema);
+const userModel = mongoose.model('user',userSchema);
+
+function errorFn(err){
+    console.log('Error found');
+    console.error(err);
+}
+
+/* ACTUAL CODE FOR MAIN PAGE:
+    server.get('/', function(req,resp){
+        resp.render('main',{
+            layout: 'index',
+            title: 'Home | Coffee Lens'
+        });
+    }); 
+*/
+
+// for testing retrieval of data
 server.get('/', function(req,resp){
-    resp.render('main',{
-        layout: 'index',
-        title: 'Home | Coffee Lens'
-    });
+    const searchQuery = {};
+    cafeModel.find(searchQuery).lean().then(function(cafes){
+        commentModel.find(searchQuery).lean().then(function(comments){
+            userModel.find(searchQuery).lean().then(function(users){
+                postModel.find(searchQuery).lean().then(function(posts){
+                    resp.render('main',{
+                        layout            : 'index',
+                        title             : 'Home | Coffee Lens',
+                        'cafe-data'       : cafes,
+                        'comments-data'   : comments,
+                        'user-data'       : users, 
+                        'post-data'       : posts
+                        });
+                }).catch(errorFn);  // postmodel fn
+            }).catch(errorFn);  // usermodel fn
+        }).catch(errorFn);      //commentmodel fn
+    }).catch(errorFn); // cafemodel fn
 });
 
 server.get('/login', function(req,resp){
@@ -48,7 +119,6 @@ server.get('/edit_profile', function(req,resp){
         title: 'Edit Profile | Coffee Lens'
     });
 }); 
-
 
 server.get('/about', function(req,resp){
     resp.render('about',{
