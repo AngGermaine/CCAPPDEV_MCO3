bafddad73ad4a25032f3d3aab8edb02394128e28
mabayawa <145282273+mabayawa@users.noreply.github.com>
2024-03-13 17:41:50 +0800

    updated view-cafe, view-profile

--- CCAPDEV-Phase2-Group18/public/common/func.js
+++ CCAPDEV-Phase2-Group18/public/common/func.js
@@ -1,24 +1,4 @@
 $(document).ready(function(){
     //View Cafe
-    for(let i =1;i<=5;i++){
-        $("#likebtn-"+i).click(function(){
-            $.post('view-cafe'),{},
-                function(data,status){
-                    if(status==='success'){
-                        let likeCount = Number($("likecount-"+i).text()) + 1;
-                        $("review"+i).text(likeCount);
-                    }
-                }
-        });
-
-        $("#dislikebtn-"+i).click(function(){
-            $.post('view-cafe'),{},
-                function(data,status){
-                    if(status==='success'){
-                        let likeCount = Number($("dislikecount-"+i).text()) + 1;
-                        $("review"+i).text(likeCount);
-                    }
-                }
-        });
-    }
+    
 });
