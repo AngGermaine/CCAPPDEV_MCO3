294b71c51b0e42ee588b98affc23043701d56ed9
AngGermaine <germaine_richie_ang@dlsu.edu.ph>
2024-04-03 15:50:02 +0800

    change dates to string
    dates now saved as string in db

--- CCAPDEV-Phase2-Group18/schemas/postSchema.js
+++ CCAPDEV-Phase2-Group18/schemas/postSchema.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
     authorid: {type: Number},
-    createdate: {type: Date, default: Date.now},
-    updatedate: {type: Date, default: Date.now},
-    dateposted: {type: Date, default: Date.now},
+    createdate: {type: String},
+    updatedate: {type: String},
+    dateposted: {type: String},
     upvote: { type: Number, default: 0}, 
     downvote: {type: Number, default: 0},
     title: {type: String},
